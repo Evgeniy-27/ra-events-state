4b6ca1e5efd60f1961ac08a3a33380b58b6ae784
Evgeniy <Li.Evgen@yandex.ru>
2021-05-11 22:55:30 +0700

    task is ready

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Portfolio from './filter/Portfolio';
+import { proects, filters } from './filter/data';
+import Store from './layouts/Store';
+import products from './layouts/data/products';
+
+function App() {
+  return (
+    <div>
+      <Portfolio proects={proects} filters={filters} />
+      <Store products={products} />
+    </div>
+  );
+}
+
+export default App;
